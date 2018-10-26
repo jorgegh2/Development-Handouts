7b0e7dd51b9b02aaa31cfd1eff359ea449c9c783
jorgegh2 <jorgegh2@TR12.CAT>
2018-10-26 09:53:28 +0200

    update Dijkstra

--- Dev_class7.8_handout/Motor2D/j1Map.cpp
+++ Dev_class7.8_handout/Motor2D/j1Map.cpp
@@ -41,7 +41,8 @@ void j1Map::ResetPath()
 	frontier.Push(iPoint(19, 4), 0);
 	visited.add(iPoint(19, 4));
 	breadcrumbs.add(iPoint(19, 4));
-	memset(cost_so_far, 0, sizeof(uint) * COST_MAP * COST_MAP);
+	memset(cost_so_far, -1, sizeof(uint) * COST_MAP * COST_MAP);
+	cost_so_far[19][4] = 0;
 }
 
 void j1Map::Path(int x, int y)
@@ -71,7 +72,7 @@ void j1Map::PropagateDijkstra()
 	// on each cell (is already reset to 0 automatically)
 
 	iPoint curr;
-	int cost = MovementCost(curr.x, curr.y);
+	//int cost = MovementCost(curr.x, curr.y);
 	if (frontier.Pop(curr))
 	{
 		iPoint neighbors[4];
@@ -79,7 +80,7 @@ void j1Map::PropagateDijkstra()
 		neighbors[1].create(curr.x + 0, curr.y + 1);
 		neighbors[2].create(curr.x - 1, curr.y + 0);
 		neighbors[3].create(curr.x + 0, curr.y - 1);
-
+		
 		for (uint i = 0; i < 4; ++i)
 		{
 			int movementCost = MovementCost(neighbors[i].x, neighbors[i].y);
@@ -87,11 +88,13 @@ void j1Map::PropagateDijkstra()
 
 			int newCost = cost_so_far[curr.x][curr.y] + movementCost;
 			
-			if (cost_so_far[neighbors[i].x][neighbors[i].y] == 0 || newCost < cost_so_far[neighbors[i].x][neighbors[i].y])
+			if (cost_so_far[neighbors[i].x][neighbors[i].y] == -1 || newCost < cost_so_far[neighbors[i].x][neighbors[i].y])
 			{
 				cost_so_far[neighbors[i].x][neighbors[i].y] = newCost;
 				frontier.Push(neighbors[i], newCost);
-				breadcrumbs.add(neighbors[i]);
+				visited.add(neighbors[i]);
+				breadcrumbs.add(curr);
+				
 			}
 		}
 	}
